9c2c5c58c65544f66b363be19211c601af004ec1
b4shtirk1n <wowan4512@gmail.com>
2026-07-22 20:09:02 +0300

    UPDATE: implement leaderboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 /* eslint-disable react-hooks/immutability */
 import type { Player } from "./models/Player";
 import type { Server } from "./models/Server";
+import type { Leaderboard } from "./models/Leaderboard";
 import { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
   const [server, setServer] = useState<Server | null>(null);
   const [players, setPlayers] = useState<Player[]>([]);
+  const [leaderboard, setLeaderboard] = useState<Leaderboard[]>([]);
 
   useEffect(() => {
-    loadPlayers();
     loadServer();
+    loadPlayers();
+    loadLeaderboard();
 
     const id = setInterval(() => {
       loadServer();
       loadPlayers();
+      loadLeaderboard();
       console.log(server);
     }, 3000);
 
@@ -32,29 +36,54 @@ export default function App() {
     setPlayers(await res.json());
   }
 
+  async function loadLeaderboard() {
+    const res = await fetch("https://streamuse.app/api/leaderboard");
+    setLeaderboard(await res.json());
+  }
+
   return (
     <>
-      <h2>Игроки онлайн ({players.length})</h2>
-
-      <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-        {players.map((p) => (
-          <div
-            key={p.uuid}
-            style={{ display: "flex", gap: 20, alignItems: "center" }}
-          >
-            <img
-              width={48}
-              src={`https://i.pinimg.com/474x/64/3d/67/643d67ff020ca8b411001a6b31f5c149.jpg`}
-              alt={p.name}
-            />
-            <div>
-              <p>{p.name}</p>
+      <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+          <h2>Игроки онлайн ({players.length})</h2>
+          {players.map((p) => (
+            <div
+              key={p.uuid}
+              style={{ display: "flex", gap: 20, alignItems: "center" }}
+            >
+              <img
+                width={48}
+                src={`https://i.pinimg.com/474x/64/3d/67/643d67ff020ca8b411001a6b31f5c149.jpg`}
+                alt={p.name}
+              />
+              <div>
+                <p>{p.name}</p>
+              </div>
+              <p style={{ display: "flex", justifyContent: "center" }}>
+                Ping: {p.ping}
+              </p>
             </div>
-            <p style={{ display: "flex", justifyContent: "center" }}>
-              Ping: {p.ping}
-            </p>
-          </div>
-        ))}
+          ))}
+        </div>
+
+        <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+          <h2>Топ по отключению от сервера ({players.length})</h2>
+          {leaderboard.map((p) => (
+            <div
+              key={p.uuid}
+              style={{ display: "flex", gap: 20, alignItems: "center" }}
+            >
+              <img
+                width={48}
+                src={`https://i.pinimg.com/474x/64/3d/67/643d67ff020ca8b411001a6b31f5c149.jpg`}
+                alt={p.name}
+              />
+              <div>
+                <p>{p.name}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
       <iframe
